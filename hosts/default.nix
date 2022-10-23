96e77ab3d3c516a9dab654b2368d2d776bbf7932
Thirty Eighth and Brunswick <86792483+38B@users.noreply.github.com>
2022-10-23 14:31:25 -0400

    args

--- hosts/default.nix
+++ hosts/default.nix
@@ -1,5 +1,6 @@
+{ pkgs }:
 {
-  nixosConfigurations.proto = nixpkgs.lib.nixosSystem {
+  proto = nixpkgs.lib.nixosSystem {
     pkgs = packages.x86_64-linux;
     specialArgs = { inherit inputs; };
     modules = [./proto.nix];
